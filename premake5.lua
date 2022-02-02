6b90a46fea4540feae5898db52d9873dfdae2e87
Athang Gupte <xdevapps0@gmail.com>
2022-02-02 23:39:34 +0530

    Fixed premake file merge errors

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,11 @@
 project "ImGui"
-<<<<<<< docking
-<<<<<<< docking
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"
-
-<<<<<<< docking
-=======
-    kind "StaticLib"
-    language "C++"
     
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
->>>>>>> Added premake file.
     files
     {
         "imconfig.h",
@@ -21,6 +13,7 @@ project "ImGui"
         "imgui.cpp",
         "imgui_draw.cpp",
         "imgui_internal.h",
+        "imgui_tables.cpp",
         "imgui_widgets.cpp",
         "imstb_rectpack.h",
         "imstb_textedit.h",
@@ -28,6 +21,10 @@ project "ImGui"
         "imgui_demo.cpp"
     }
     
+    includedirs {
+        "../freetype/include"
+    }
+
     filter "system:windows"
         systemversion "latest"
         cppdialect "C++17"
@@ -35,62 +32,11 @@ project "ImGui"
         
     filter { "system:windows", "configurations:Release" }
         buildoptions "/MT"
-<<<<<<< docking
-=======
-=======
-	kind "StaticLib"
-	language "C++"
-
->>>>>>> Added support for linux
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-<<<<<<< docking
-<<<<<<< docking
-        "imgui_tables.cpp",
-=======
->>>>>>> Added support for linux
-=======
-        "imgui_tables.cpp",
->>>>>>> Update Premake
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
-
-   includedirs {
-       "../freetype/include"
-   }
 
 	filter "system:windows"
 		systemversion "latest"
-<<<<<<< docking
-<<<<<<< docking
-<<<<<<< docking
 		cppdialect "C++17"
 		staticruntime "On"
->>>>>>> Added support for linux
-=======
-		staticruntime(WinCRunTime_Type)
->>>>>>> Static Runtime variable
-=======
->>>>>>> Added premake file.
-=======
-		cppdialect "C++17"
-		staticruntime "On"
->>>>>>> Added support for linux
-=======
-		staticruntime(WinCRunTime_Type)
->>>>>>> Static Runtime variable
 
 	filter "system:linux"
 		pic "On"
@@ -104,6 +50,3 @@ project "ImGui"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
